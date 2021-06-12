e95ae9b78157b342b2542acfece71f61e74a0897
mert ekizler <mertekizler@hotmail.com>
2021-06-12 15:50:30 +0300

    ME-productDao metodları oluşturuldu

--- northwind/src/main/java/kodlamaio/northwind/dataAccess/abstracts/ProductDao.java
+++ northwind/src/main/java/kodlamaio/northwind/dataAccess/abstracts/ProductDao.java
@@ -1,11 +1,15 @@
 package kodlamaio.northwind.dataAccess.abstracts;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import kodlamaio.northwind.entities.concretes.Product;
 
 public interface ProductDao extends JpaRepository<Product,Integer>{
 
-	
-	
+	Product getByProductName(String productName);
+	Product getByProductNameAndCategoryId(String productName, int categoryId);
+	List<Product> getByProductNameOrCategoryId(String productName, int categoryId);
+	List<Product> getByCategoryIdIn(List<Integer> categories);
 }
--- northwind/src/main/java/kodlamaio/northwind/entities/concretes/Product.java
+++ northwind/src/main/java/kodlamaio/northwind/entities/concretes/Product.java
@@ -5,6 +5,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
@@ -16,8 +18,8 @@ public class Product {
 	@Column(name="product_id")
 	private int id;
 	
-	@Column(name="category_id")
-	private int categoryId;
+	//@Column(name="category_id")
+	//private int categoryId;
 	
 	@Column(name="product_name")
 	private String productName;
@@ -31,17 +33,20 @@ public class Product {
 	@Column(name="quantity_per_unit")
 	private String quantityPerUnit;
 	
+	@ManyToOne()
+	@JoinColumn(name="category_id")
+	private Category category;
 	
 	public Product() {
 		super();
 	}
 
 
-	public Product(int id, int categoryId, String productName, double unitPrice, short unitsInStock,
+	public Product(int id, String productName, double unitPrice, short unitsInStock,
 			String quantityPerUnit) {
 		super();
 		this.id = id;
-		this.categoryId = categoryId;
+		//this.categoryId = categoryId;
 		this.productName = productName;
 		this.unitPrice = unitPrice;
 		this.unitsInStock = unitsInStock;
@@ -56,16 +61,6 @@ public class Product {
 
 	public void setId(int id) {
 		this.id = id;
-	}
-
-
-	public int getCategoryId() {
-		return categoryId;
-	}
-
-
-	public void setCategoryId(int categoryId) {
-		this.categoryId = categoryId;
 	}
 
 
